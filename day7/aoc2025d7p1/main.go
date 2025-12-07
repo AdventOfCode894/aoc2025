f56bd3618a8dd4af035a57024c07209e0d5b0986
AdventOfCode894 <95511313+AdventOfCode894@users.noreply.github.com>
2025-12-07 00:14:01 -0500

    Day 7: raw solutions

---
+++ day7/aoc2025d7p1/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"io"
+
+	"github.com/AdventOfCodee894/aoc2025/internal/aocio"
+	"github.com/AdventOfCodee894/aoc2025/internal/aocmain"
+)
+
+func main() {
+	aocmain.HandlePuzzle(solvePuzzle)
+}
+
+func solvePuzzle(r io.Reader) (int, error) {
+	pr := aocio.NewPuzzleReader(r)
+
+	var tachyonEntries []bool
+	splits := 0
+	for pr.NextLine() {
+		manifoldLine := pr.LineRunes()
+		if len(tachyonEntries) < 1 {
+			tachyonEntries = make([]bool, len(manifoldLine))
+			for i, b := range manifoldLine {
+				if b == 'S' {
+					tachyonEntries[i] = true
+				}
+			}
+			continue
+		}
+		for i, b := range manifoldLine {
+			if b != '^' {
+				continue
+			}
+			if tachyonEntries[i] {
+				if !tachyonEntries[i-1] {
+					tachyonEntries[i-1] = true
+				}
+				if !tachyonEntries[i+1] {
+					tachyonEntries[i+1] = true
+				}
+				tachyonEntries[i] = false
+				splits++
+			}
+		}
+	}
+
+	return splits, nil
+}
---
+++ day7/aoc2025d7p2/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"io"
+
+	"github.com/AdventOfCodee894/aoc2025/internal/aocio"
+	"github.com/AdventOfCodee894/aoc2025/internal/aocmain"
+)
+
+func main() {
+	aocmain.HandlePuzzle(solvePuzzle)
+}
+
+func solvePuzzle(r io.Reader) (int, error) {
+	pr := aocio.NewPuzzleReader(r)
+
+	var tachyonWorlds []int
+	for pr.NextLine() {
+		manifoldLine := pr.LineRunes()
+		if len(tachyonWorlds) < 1 {
+			tachyonWorlds = make([]int, len(manifoldLine))
+			for i, b := range manifoldLine {
+				if b == 'S' {
+					tachyonWorlds[i] = 1
+				}
+			}
+			continue
+		}
+		for i, b := range manifoldLine {
+			if b != '^' {
+				continue
+			}
+			tachyonWorlds[i-1] += tachyonWorlds[i]
+			tachyonWorlds[i+1] += tachyonWorlds[i]
+			tachyonWorlds[i] -= tachyonWorlds[i]
+		}
+	}
+
+	timelines := 0
+	for _, worlds := range tachyonWorlds {
+		timelines += worlds
+	}
+
+	return timelines, nil
+}
